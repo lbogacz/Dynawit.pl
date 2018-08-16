43f58b0ddd419bb98e2b3821058d66113f0e08ba
lbogacz <lukas.bogacz@gmail.com>
2018-08-16 23:04:32 +0100

    added main files

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const hamburger = document.querySelector('.hamburger');
+
+hamburger.addEventListener('click', function (event) {
+
+    const menu = document.querySelector('.main-nav-list ul');
+
+    menu.classList.toggle('open');
+
+})
+
